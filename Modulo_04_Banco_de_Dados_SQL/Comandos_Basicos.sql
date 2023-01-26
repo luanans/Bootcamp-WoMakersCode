d4ec25d3c5c4e21a384d956a9869a274e60f5d01
Luana Nunes da Silva <luananunesdasilva@MacBook-Pro-de-Luana.local>
2023-01-25 21:21:36 -0300

    Rascunho aula 25/01

--- Modulo_04_Banco_de_Dados_SQL/Comandos_Basicos.sql
+++ Modulo_04_Banco_de_Dados_SQL/Comandos_Basicos.sql
@@ -14,3 +14,35 @@ INSERT INTO sakila.film
 
 INSERT INTO sakila.film_actor
 (film_id, actor_id) VALUES (1001, 201)
+
+
+-- A definição da nossa tabela é a seguinte: 
+order_id SMALLINT UNSIGNED PRIMARY KEY (id do pedido)
+store_id TINYINT UNSIGNEDNOT NULL (qual loja fez o pedido)
+film_id SMALLINT UNSIGNED NOT NULL (qual filme foi pedido)
+ordered_amount SMALLINT UNSIGNED NOT NULL (quantos filmes foram pedidos)
+price DECIMAL(10,3) NOT NULL (quanto custa o pedido)
+order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+CREATE TABLE sakila.order (
+    order_id INT NOT NULL AUTO_INCREMENT,
+    store_id TINYINT(255) UNSIGNED NOT NULL,
+    film_id SMALLINT(255) UNSIGNED NOT NULL,
+    ordered_amount SMALLINT(255) UNSIGNED NOT NULL,
+    price DECIMAL(10, 3) UNSIGNED NOT NULL,
+    order_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (order_id)
+);
+
+ALTER TABLE sakila.order
+ADD INDEX fk_store_id_idx (store_id ASC) VISIBLE,
+ADD INDEX fk_film_id_idx (film_id ASC) VISIBLE,
+ADD CONSTRAINT fk_store_id
+FOREIGN KEY (store_id)
+REFERENCES sakila.store (store_id)
+ON DELETE RESTRICT
+ON DELETE CASCADE, 
+ADD CONSTRAINT fk_film_id
+FOREIGN KEY (film_id)
+ON DELETE RESTRICT
+ON DELETE CASCADE;
